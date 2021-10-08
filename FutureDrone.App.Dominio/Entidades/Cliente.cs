828f42d8714fd7d0f9761049cfbf8d58f3ad1d1f
Andres Hernandez <hilanandreshernandez1@gmail.com>
2021-10-08 18:46:04 -0500

    prueba2

--- FutureDrone.App.Dominio/Entidades/Cliente.cs
+++ FutureDrone.App.Dominio/Entidades/Cliente.cs
@@ -1,7 +1,7 @@
 using System;
 namespace FutureDrone.App.Dominio
 {
-    public class Cliente
+    public class Cliente ///
     {
         public int Id {get;set;}
         public string Nombre {get;set;}
